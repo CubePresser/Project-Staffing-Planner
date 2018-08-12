cf97227ebc18a1285707da5b6f424a1d81ae32ca
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2018-08-12 16:32:42 -0700

    Modularized table fill code
    Created the queryLoopConnector to remove redundant code chunks from home.js which takes care of setting up and maintaining query loops.

--- routes/home.js
+++ routes/home.js
@@ -29,8 +29,67 @@ function isSQLError(res, error) {
     }
 }
 
+/*************************************************
+ * Function: queryLoopConnector
+ * Description: Executes an SQL query then runs a loop of subsequent queries on the data obtained from the initial SQL query
+ * Params: response, mysql module, object with options {sql query, inserts, query to append data to, name of key for appended data, query to run on initial SQL query, callback function that gets a boolean success value}
+ * Returns: none
+ * Pre-conditions: Must provide everything but the inserts (if your query does not need it)
+ * Post-conditions: All callbacks have finished before this function has completed and all sql data obtained
+ * **********************************************/
+function queryLoopConnector(res, mysql, options)
+{
+    //Run query from options
+    mysql.pool.query(options.sql, options.inserts, function(error, table, fields) {
+        if(isSQLError(res, error)) //Check for sql errors
+        {
+            options.callback(false); //Callback with a failure status on SQL error
+        }
+        else //If no SQL errors
+        {
+            var rows = table.length; //How many rows need to be evaluated?
+            var count = 0; //Keeps track of how many rows have been evaluated
+            var status = true; //True if no errors, false otherwise
 
-function getMonthsManpower(res, pr, mysql, callback) {
+            if(rows == 0) //If the table returned is empty
+            {
+                options.callback(true); //No useful data was obtained but no errors occured so callback with success
+            }
+            else 
+            {
+                //Enumerate through the rows of the returned SQL query and run the 'next' query on that returned data
+                table.forEach(function(row) 
+                { 
+                    //For each enumeration, run the 'next' query on that data
+                    options.next(res, mysql, row, function(success) {
+                        if(!success) //If an error occured for any one of the 'next' queries, status to false (Errors occurred)
+                            status = false;
+
+                        count++; //Update number of rows evaluated
+
+                        //If all rows have been evaluated
+                        if(count >= rows)
+                        {
+                            options.key_container[options.key_name] = table; //Update key_container with a new key_name that references the queried data
+                            options.callback(status);
+                        }
+                    }); 
+                });  
+            }  
+        }
+        
+    });
+}
+
+/*************************************************
+ * Function: getMonthsManpower
+ * Description: Gets the manpower over the year for the project role passed in
+ * Params: response, mysql module, project role data (individual), callback function that gets a boolean success value
+ * Returns: none
+ * Pre-conditions: Valid project role object should be passed in or error will be thrown
+ * Post-conditions: If successful, appends queried data to project role
+ * **********************************************/
+function getMonthsManpower(res, mysql, pr, callback) {
     sql = `
         select month.id, ifnull(projectrole_month.manpower, 0) as mp
         from month
@@ -44,12 +103,20 @@ function getMonthsManpower(res, pr, mysql, callback) {
         }
         else 
         {
-            pr.manpower = mp_result;
+            pr.manpower = mp_result; //Append month data to project role object
             callback(true);
         }
     });
 }
 
+/*************************************************
+ * Function: getListOfProjectRoles
+ * Description: Queries for a list of all project roles for the specified project and their total cost over the year
+ * Params: Response, mysql module, project data (individual)
+ * Returns: none
+ * Pre-conditions: Valid project object should be passed in or error will be thrown 
+ * Post-conditions: If successful, appends queried data to project role
+ * **********************************************/
 function getListOfProjectRoles(res, mysql, project, callback) {
     sql = `
         select project_role.id as id, team.name as team_name, role.name as role_name, company.name as company_name, location.name as location_name, costs.total_cost as total_cost
@@ -70,49 +137,25 @@ function getListOfProjectRoles(res, mysql, project, callback) {
         where project_role.project_id = ?
         order by id;
     `;
-    mysql.pool.query(sql, [project.id], function(error, project_roles, fields) {
-        if(isSQLError(res, error))
-        {
-            callback(false);
-        }
-        else
-        {
-            var len = project_roles.length;
-            var count = 0;
-            var status = true;
-            
-            if(len == 0) //If there are no project roles associated with the current project
-            {
-                callback(true);
-            }
-            else 
-            {
-                project_roles.forEach(function(pr) {
-                    getMonthsManpower(res, pr, mysql, function(success) {
-                        if(!success)
-                            status = false;
 
-                        count++;
-                        if(count >= len)
-                        {
-                            project.project_role = project_roles;
-                            callback(status);
-                        }
-                    });
-                });    
-            }
-        }
-        
+    //Get the yearly manpower for each project role obtained from the above query
+    queryLoopConnector(res, mysql, {
+        sql           : sql,
+        inserts       : [project.id], 
+        key_container : project, 
+        key_name      : "project_role", 
+        next          : getMonthsManpower, 
+        callback      : callback
     });
 }
 
 /*************************************************
- * Function: 
- * Description: 
- * Params: 
- * Returns: 
- * Pre-conditions: 
- * Post-conditions: 
+ * Function: getProjectRolesList
+ * Description: Obtains data necessary to fill the table at the top of each page on the website. This function first obtains a list of all projects that have project roles then runs a series of queries on them to obtain the necessary information.
+ * Params: Response, mysql module, handlebars contextual object for filling variables, callback to complete (For the '/' get)
+ * Returns: none
+ * Pre-conditions: Page has not yet been rendered. Connection to SQL server established.
+ * Post-conditions: Handlebars contextual object has been filled with all the successful data
  * **********************************************/
 function getProjectRolesList(res, mysql, context, complete) {
     sql = `
@@ -120,40 +163,12 @@ function getProjectRolesList(res, mysql, context, complete) {
         from project 
         inner join project_role on project_role.project_id = project.id
     `;
-    mysql.pool.query(sql, function(error, projects, fields) {
-        if(isSQLError(res, error))
-        {
-            complete(false);
-        }
-        else
-        {
-            var len = projects.length;
-            var count = 0;
-            var status = true;
-
-            if(len == 0) //If there are no projects
-            {
-                complete(true);
-            }
-            else 
-            {
-                projects.forEach(function(project) 
-                { 
-                    getListOfProjectRoles(res, mysql, project, function(success) {
-                        if(!success)
-                            status = false;
-
-                        count++;
-                        if(count >= len)
-                        {
-                            context.project_name = projects;
-                            complete(status);
-                        }
-                    }); 
-                });  
-            }  
-        }
-        
+    queryLoopConnector(res, mysql, {
+        sql           : sql,
+        key_container : context, 
+        key_name      : "project_name", 
+        next          : getListOfProjectRoles, 
+        callback      : complete
     });
 }
 
