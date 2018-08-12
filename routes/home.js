730b15d455da31560a064026032f9ff5b3ddbde0
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2018-08-12 14:24:25 -0700

    Error checks improved
    App no longer crashes on an sql error. Error message is displayed.

--- routes/home.js
+++ routes/home.js
@@ -7,18 +7,30 @@
 var express = require('express');
 var router = express.Router();
 
-function isSQLError(error) {
+/*************************************************
+ * Function: isSQLError
+ * Description: Checks for an sql error from the argument passed in.
+ * Params: Response object, error object from sql query
+ * Returns: True if an error has occured, false otherwise
+ * Pre-conditions: SQL Query has already run and returned an error object to evaluate.
+ * Post-conditions: Error object has been evaluated, messages written if needed and error status returned. Preferably, the server remains operational after this error and it is handled accordingly.
+ * **********************************************/
+function isSQLError(res, error) {
     if(error)
     {
         let message = "SQL Error: " + error.sqlMessage;
         console.log(message);
         res.write(message);
+        return true;
+    }
+    else 
+    {
+        return false;
     }
 }
 
 
-function getMonthsManpower(res, pr, mysql, callback)
-{
+function getMonthsManpower(res, pr, mysql, callback) {
     sql = `
         select month.id, ifnull(projectrole_month.manpower, 0) as mp
         from month
@@ -26,13 +38,19 @@ function getMonthsManpower(res, pr, mysql, callback)
         order by month.id;
     `;
     mysql.pool.query(sql, [pr.id], function(error, mp_result, fields) {
-        isSQLError(error);
-        pr.manpower = mp_result;
-        callback();
+        if(isSQLError(res, error)) 
+        {
+           callback(false);
+        }
+        else 
+        {
+            pr.manpower = mp_result;
+            callback(true);
+        }
     });
 }
 
-function getListOfProjectRoles(res, project, mysql, callback) {
+function getListOfProjectRoles(res, mysql, project, callback) {
     sql = `
         select project_role.id as id, team.name as team_name, role.name as role_name, company.name as company_name, location.name as location_name
         from project_role
@@ -44,24 +62,36 @@ function getListOfProjectRoles(res, project, mysql, callback) {
         order by id;
     `;
     mysql.pool.query(sql, [project.id], function(error, project_roles, fields) {
-        isSQLError(error);
-        var len = project_roles.length;
-        var count = 0;
-        if(len == 0) //If there are no project roles associated with the current project
+        if(isSQLError(res, error))
         {
-            callback();
+            callback(false);
         }
-        else {
-            project_roles.forEach(function(pr) {
-                getMonthsManpower(res, pr, mysql, function() {
-                    count++;
-                    if(count >= len)
-                    {
-                        project.project_role = project_roles;
-                        callback();
-                    }
-                });
-            });    
+        else
+        {
+            var len = project_roles.length;
+            var count = 0;
+            var status = true;
+            
+            if(len == 0) //If there are no project roles associated with the current project
+            {
+                callback(true);
+            }
+            else 
+            {
+                project_roles.forEach(function(pr) {
+                    getMonthsManpower(res, pr, mysql, function(success) {
+                        if(!success)
+                            status = false;
+
+                        count++;
+                        if(count >= len)
+                        {
+                            project.project_role = project_roles;
+                            callback(status);
+                        }
+                    });
+                });    
+            }
         }
         
     });
@@ -82,111 +112,136 @@ function getProjectRolesList(res, mysql, context, complete) {
         inner join project_role on project_role.project_id = project.id
     `;
     mysql.pool.query(sql, function(error, projects, fields) {
-        isSQLError(error);
-        var len = projects.length;
-        var count = 0;
-        if(len == -1) //If there are no projects
+        if(isSQLError(res, error))
         {
-            complete();
+            complete(false);
         }
-        else {
-            projects.forEach(function(project) 
-            { 
-                getListOfProjectRoles(res, project, mysql, function() {
-                    count++;
-                    if(count >= len)
-                    {
-                        context.project_name = projects;
-                        complete();
-                    }
-                }); 
-            });  
-        }  
+        else
+        {
+            var len = projects.length;
+            var count = 0;
+            var status = true;
+
+            if(len == 0) //If there are no projects
+            {
+                complete(true);
+            }
+            else 
+            {
+                projects.forEach(function(project) 
+                { 
+                    getListOfProjectRoles(res, mysql, project, function(success) {
+                        if(!success)
+                            status = false;
+
+                        count++;
+                        if(count >= len)
+                        {
+                            context.project_name = projects;
+                            complete(status);
+                        }
+                    }); 
+                });  
+            }  
+        }
+        
     });
 }
 
-function getProjects(res, mysql, context, complete) {
-    mysql.pool.query("select * from project", function(error, results, fields) {
-        isSQLError(error);
-        context.project = results;
-        complete();
+function getTable(sql, res, mysql, context, item_name, complete)
+{
+    mysql.pool.query(sql, function(error, results, fields) {
+        if(isSQLError(res, error))
+        {
+            complete(false);
+        }
+        else
+        {
+            context[item_name] = results;
+            complete(true);
+        }
+        
     });
 }
 
-function getTeams(res, mysql, context, complete) {
-    mysql.pool.query("select * from team", function(error, results, fields) {
-        isSQLError(error);
-        context.team = results;
-        complete();
-    });
-}
-
-function getRoles(res, mysql, context, complete) {
-    mysql.pool.query("select * from role", function(error, results, fields) {
-        isSQLError(error);
-        context.role = results;
-        complete();
-    });
-}
-
-function getCompanies(res, mysql, context, complete) {
-    mysql.pool.query("select * from company", function(error, results, fields) {
-        isSQLError(error);
-        context.company = results;
-        complete();
-    });
-}
-
-function getLocations(res, mysql, context, complete) {
-    mysql.pool.query("select * from location", function(error, results, fields) {
-        isSQLError(error);
-        context.location = results;
-        complete();
-    });
-}
-
-function getProjectRoles(res, mysql, context, complete) {
-    mysql.pool.query("select * from project_role order by id", function(error, results, fields) {
-        isSQLError(error);
-        context.project_role = results;
-        complete();
-    });
-}
-
-function totalCostResponse(res, mysql, sql, inserts) {
+function totalCostResponse(res, mysql, sql, inserts, callback) {
     mysql.pool.query(sql, inserts, function(error, results, fields) {
-        isSQLError(error);
-        var total_cost = results[0].total_cost;
-        res.write("Total Cost: $" + (total_cost == null ? 0 : total_cost));
-        res.end();
+        if(isSQLError(res, error))
+        {
+            callback(false);
+        }
+        else
+        {
+            var total_cost = results[0].total_cost;
+            res.write("Total Cost: $" + (total_cost == null ? 0 : total_cost));
+            callback(true);
+        }
+        
     });
 }
 
-//TODO: Fill the project roles area (Get info from db) and render the page
 router.get('/', function(req, res) {
     var mysql = req.app.get('mysql');
 
     var callbackCount = 0; //Counter to ensure that all callbacks have returned before rendering
+
+    var status = true; //Keeps track of the error status
 
     var context = {}; //Handlebars info
     context.title = "Home";
     context.scripts = ["home-ajax.js"];
 
     getProjectRolesList(res, mysql, context, complete);
-    getProjects(res, mysql, context, complete);
-    getTeams(res, mysql, context, complete);
-    getRoles(res, mysql, context, complete);
-    getCompanies(res, mysql, context, complete);
-    getLocations(res, mysql, context, complete);
-    getProjectRoles(res, mysql, context, complete);
+    //Gets project table to fill dropdown
+    getTable(
+        "select * from project order by name",
+        res, mysql, context, "project", complete
+    );
+    //Gets team table to fill dropdown
+    getTable(
+        "select * from team order by name",
+        res, mysql, context, "team", complete
+    );
+    //Gets role table to fill dropdown
+    getTable(
+        "select * from role order by name",
+        res, mysql, context, "role", complete
+    );
+    //Gets company table to fill dropdown
+    getTable(
+        "select * from company order by name",
+        res, mysql, context, "company", complete
+    );
+    //Gets location table to fill dropdown
+    getTable(
+        "select * from location order by name",
+        res, mysql, context, "location", complete
+    );
+    //Gets project_role table to fill dropdown
+    getTable(
+        "select * from project_role order by id",
+        res, mysql, context, "project_role", complete
+    );
 
     //Waits for the completion of all callback functions then renders the page
-    function complete() {
+    function complete(success) {
+        if(!success)
+            status = false;
+
         callbackCount++;
-        if(callbackCount >= 7)
+        if(callbackCount >= 7) //Wait for all callbacks to finish
         {
-            console.log("Rendered home page");
-            res.render('home', context);
+            //Check for errors
+            if(status) //No errors
+            {
+                res.render('home', context);
+            }
+            else //Error has occured
+            {
+                res.status(400);
+                res.end();
+            }
+            
         }
     }
 });
@@ -206,7 +261,18 @@ router.post('/q0', function(req, res) {
         ) project_role_costs
     `;
     var inserts = [req.body.project, req.body.month];
-    totalCostResponse(res, mysql, sql, inserts);
+    totalCostResponse(res, mysql, sql, inserts, function(success) {
+        if(success)
+        {
+            res.status(200);
+            res.end();
+        }
+        else
+        {
+            res.status(400);
+            res.end();
+        }
+    });
 });
 
 //Query 1: Total cost of all instances of a role in a single project
@@ -225,7 +291,18 @@ router.post('/q1', function(req, res) {
         where pr_pid = ? and pr_rid = ?
     `;
     var inserts = [req.body.project, req.body.role];
-    totalCostResponse(res, mysql, sql, inserts);
+    totalCostResponse(res, mysql, sql, inserts, function(success) {
+        if(success)
+        {
+            res.status(200);
+            res.end();
+        }
+        else
+        {
+            res.status(400);
+            res.end();
+        }
+    });
 });
 
 //Query 2: Total cost of a company for a single project
@@ -244,7 +321,18 @@ router.post('/q2', function(req, res) {
         where pr_pid = ? and pr_cid = ?
     `;
     var inserts = [req.body.project, req.body.company];
-    totalCostResponse(res, mysql, sql, inserts);
+    totalCostResponse(res, mysql, sql, inserts, function(success) {
+        if(success)
+        {
+            res.status(200);
+            res.end();
+        }
+        else
+        {
+            res.status(400);
+            res.end();
+        }
+    });
 });
 
 //Query 3: Roles associated with a single company
@@ -255,13 +343,14 @@ router.post('/q3', function(req, res) {
     `;
     var inserts = [];
     mysql.pool.query(sql, inserts, function(error, results, fields) {
-        if(error)
+        if(isSQLError(res, error))
         {
-            res.write("SQL Error: " + error.sqlMessage);
+            res.status(400);
             res.end();
         }
-        else {
-            res.write(results);
+        else
+        {
+            res.status(200);
             res.end();
         }
         
@@ -276,13 +365,14 @@ router.post('/q4', function(req, res) {
     `;
     var inserts = [];
     mysql.pool.query(sql, inserts, function(error, results, fields) {
-        if(error)
+        if(isSQLError(res, error))
         {
-            res.write("SQL Error: " + error.sqlMessage);
+            res.status(400);
             res.end();
         }
-        else {
-            res.write(results);
+        else
+        {
+            res.status(200);
             res.end();
         }
     });
@@ -296,13 +386,14 @@ router.post('/q5', function(req, res) {
     `;
     var inserts = [];
     mysql.pool.query(sql, inserts, function(error, results, fields) {
-        if(error)
+        if(isSQLError(res, error))
         {
-            res.write("SQL Error: " + error.sqlMessage);
+            res.status(400);
             res.end();
         }
-        else {
-            res.write(results);
+        else
+        {
+            res.status(200);
             res.end();
         }
     });
@@ -316,13 +407,14 @@ router.post('/q6', function(req, res) {
     `;
     var inserts = [];
     mysql.pool.query(sql, inserts, function(error, results, fields) {
-        if(error)
+        if(isSQLError(res, error))
         {
-            res.write("SQL Error: " + error.sqlMessage);
+            res.status(400);
             res.end();
         }
-        else {
-            res.write(results);
+        else
+        {
+            res.status(200);
             res.end();
         }
     });
@@ -336,13 +428,14 @@ router.post('/q7', function(req, res) {
     `;
     var inserts = [];
     mysql.pool.query(sql, inserts, function(error, results, fields) {
-        if(error)
+        if(isSQLError(res, error))
         {
-            res.write("SQL Error: " + error.sqlMessage);
+            res.status(400);
             res.end();
         }
-        else {
-            res.write(results);
+        else
+        {
+            res.status(200);
             res.end();
         }
     });
